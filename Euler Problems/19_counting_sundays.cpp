ffe05990f983dfd723f17609d195b931d20a204d
kindly-framing <saa224@njit.edu>
2022-06-23 14:55:51 -0400

    Problem 19 Euler

--- Euler Problems/19_counting_sundays.cpp
+++ Euler Problems/19_counting_sundays.cpp
@@ -26,61 +26,135 @@
 #include <vector>
 
 class Calendar {
-private:
-  std::string days[7] = {"Sunday",   "Monday", "Tuesday", "Wednesday",
-                         "Thursday", "Friday", "Saturday"};
-  int date;
-  int month;
-  int year;
-  std::string day;
+  private:
+    std::string days[7] = {"Sunday",   "Monday", "Tuesday", "Wednesday",
+                           "Thursday", "Friday", "Saturday"};
+    int date;
+    int month;
+    int year;
+    int day_index;
 
-public:
-  Calendar(int date_, int month_, int year_, std::string day_);
-  ~Calendar();
+  public:
+    Calendar(int date_, int month_, int year_, std::string day_);
+    ~Calendar();
 
-  int date();
-  int month();
-  int year();
-  std::string day();
+    int get_date();
+    int get_month();
+    int get_year();
+    std::string get_day();
 
-  void next_day();
+    bool is_leap_year();
+
+    void next_month();
+    void next_day();
+    void next_year();
+    void display_date();
 };
 
-int main() {
-  Calendar euler(1, 1, 1900, "Monday");
+int main()
+{
+    Calendar euler(1, 1, 1901, "Tuesday");
 
-  int current_day = euler.date();
-  int current_month = euler.month();
-  int current_year = euler.year();
+    int current_day = euler.get_date();
+    int current_month = euler.get_month();
+    int current_year = euler.get_year();
 
-  int first_sundays = 0;
-  while (current_day != 1 && current_month != 1 && current_year != 2001) {
-    if (current_day == 1 && euler.day() == "Sunday") {
-      first_sundays++;
+    int first_sundays = 0;
+    while (current_year != 2001) {
+        if (current_day == 1 && euler.get_day() == "Sunday") {
+            euler.display_date();
+            first_sundays++;
+        }
+
+        euler.next_day();
+        current_day = euler.get_date();
+        current_month = euler.get_month();
+        current_year = euler.get_year();
     }
 
-    euler.next_day();
-    current_day = euler.date();
-    current_month = euler.month();
-    current_year = euler.year();
-  }
+    std::cout << "The amount of Sundays on the first are " << first_sundays
+              << "\n";
 
-  std::cout << " The amount of Sundays on the first are " << first_sundays
-            << "\n";
-
-  return 0;
+    return 0;
 }
 
-Calendar::Calendar(int date_, int month_, int year_, std::string day_) {
-  date = date_;
-  month = month_;
-  year = year_;
-  day = day_;
+Calendar::Calendar(int date_, int month_, int year_, std::string day_)
+{
+    date = date_;
+    month = month_;
+    year = year_;
+
+    for (int i = 0; i < 7; i++) {
+        if (days[i] == day_) {
+            day_index = i;
+            break;
+        }
+    }
 }
 
 Calendar::~Calendar() {}
 
-int date() { return date; }
-int month() { return month; }
-int year() { return year; }
-std::string day() { return day; }
+int Calendar::get_date() { return date; }
+int Calendar::get_month() { return month; }
+int Calendar::get_year() { return year; }
+std::string Calendar::get_day() { return days[day_index]; }
+
+bool Calendar::is_leap_year()
+{
+    return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0);
+}
+
+void Calendar::next_year()
+{
+    month = 1;
+    year++;
+}
+
+void Calendar::next_month()
+{
+    date = 1;
+    month++;
+
+    if (month == 13) {
+        next_year();
+    }
+}
+
+void Calendar::next_day()
+{
+    date++;
+    day_index++;
+
+    if (day_index == 7) {
+        day_index = 0;
+    }
+
+    // Thirty days has September, April, June and November.
+    if (month == 9 || month == 4 || month == 6 || month == 11) {
+        if (date == 31) {
+            next_month();
+        }
+    }
+    // Saving February alone, which has twenty-eight, rain or shine. And on leap
+    // years, twenty-nine.
+    else if (month == 2) {
+        if (!is_leap_year() && date == 29) {
+            next_month();
+        }
+        if (is_leap_year() && date == 30) {
+            next_month();
+        }
+    }
+    // All the rest have thirty-one
+    else {
+        if (date == 32) {
+            next_month();
+        }
+    }
+}
+
+void Calendar::display_date()
+{
+    std::cout << days[day_index] << " " << month << ", " << date << ", " << year
+              << "\n";
+}
